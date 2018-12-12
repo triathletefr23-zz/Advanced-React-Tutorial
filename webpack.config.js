20de1f148cb1637df2ff7859356fd4f5cf285c40
Pavlo LIAHUSHYN <ur5evw12@gmail.com>
2018-12-12 22:37:05 +0100

    Add configured webpack for babel7 + first API class + tests with jest

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+        entry: ['@babel/polyfill', './lib/components/Index.js'],
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: 'babel-loader',
+            }
+        ]
+    }
+};
